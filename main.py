a3e1378487a5da1facff9565ff7cd9504be9d4d1
batmaninrobin <malikmui@vcu.edu>
2022-02-15 19:47:53 -0500

    dealing with import errors???

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import numpy
-import pandas
+import numpy as np
+# import pandas as pd
 import matplotlib
 from matplotlib import pyplot as plt
 import os
 import sys
 import time
+from PIL import Image
+import yaml
 
 import pycuda
 import cupy
+
+def main(config_file):
+    print("hello world")
+    conf = yaml.safe_load(config_file)
+    img = Image.open()
+    print(img)
+    
+if __name__ == "__main__":
+    main()
