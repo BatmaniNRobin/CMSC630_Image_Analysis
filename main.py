a3286ac825d6743fc339996e5056c4005f7d7aa4
batmaninrobin <malikmui@vcu.edu>
2022-02-17 10:31:25 -0500

    dealing with imports and conda smh

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tokenize import String
 import numpy as np
 # import pandas as pd
 import matplotlib
@@ -7,14 +8,21 @@ import sys
 import time
 from PIL import Image
 import yaml
+# import toml would the stricter typing of toml be easier?
 
-import pycuda
-import cupy
+# import pycuda
+# import cupy
+    
+def read_yaml(config_file):
+    with open(config_file, "r") as f:
+        return yaml.safe_load(f)
 
-def main(config_file):
+def main():
     print("hello world")
-    conf = yaml.safe_load(config_file)
-    img = Image.open()
+    config_file = "config.yaml"
+    safe_conf = read_yaml(config_file)
+    
+    img = Image.open(dataset/)
     print(img)
     
 if __name__ == "__main__":
