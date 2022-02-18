9c22eb6e2b2be9e408c7b49ceeb4c9dbcc95e7be
batmaninrobin <malikmui@vcu.edu>
2022-02-18 17:21:32 -0500

    files names acquired

--- main.py
+++ main.py
@@ -23,29 +23,19 @@ def read_yaml(config_file):
 def read_image(image_file):
     return Image.open(image_file)
 
-def get_file_names(data_loc):
-    file_names = []
-    p = Path.Path(data_loc)
-    
-    # TODO make this actually iterate
-    for f in p.iterdir():
-        file_names[f] = f
-    return file_names
-    
-    # TODO this might work too, worth looking into
-    # tmp = Path('tmp')
-    # dgen = tmp.iterdir()
-    # list(dgen)
 
 def main():
     global safe_conf
     config_file = "config.yaml"
     safe_conf = read_yaml(config_file)
     
-    data_loc = safe_conf['DIRECTORIES']['DATA_DIR']
-    win_data_loc = safe_conf['DIRECTORIES']['WIN_DATA_DIR']
+    data_loc = Path.Path(safe_conf['DIRECTORIES']['DATA_DIR'])
+    # data_loc = safe_conf['DIRECTORIES']['WIN_DATA_DIR']
     
-    file_names = get_file_names(win_data_loc)
+    # glob("*.bmp") uses regex which slows this down, nothing else is in the dir so dont need to use it
+    files = list(data_loc.iterdir())
+    
+    
     
 
 
