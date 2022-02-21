133bc073ea3f938f42092ec054a6c91ede418f80
batmaninrobin <malikmui@vcu.edu>
2022-02-21 08:50:33 -0500

    salt and pepper

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib
 import yaml
 # import pandas as pd
+import random
 
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -18,10 +19,10 @@ def read_yaml(config_file):
     
 def read_image(image_file):
     with Image.open(image_file) as img:
-        if(isGPU):
-            return cp.array(img)
-        else:
-            return np.array(img)
+        # if(isGPU):
+        #     return cp.array(img)
+        # else:
+        return np.array(img)
 
 def convert_image_to_single_channel(color_img, choice):
     # this only works for python 3.10
@@ -40,25 +41,25 @@ def convert_image_to_single_channel(color_img, choice):
     elif(choice == 'blue'):
         return color_img[:,:,2]
 
-def kernel_calc_histogram(image_file, L):
-    import cupy as cp
-    y = cp.RawKernel(r'''
-                __global__ void calcHistogram(unsigned char *data, int width, int numPixels, int *histogram) {
-                    int tid = blockDim.x * blockIdx.x + threadIdx.x;
-                    int row = tid / width;
-                    int column = tid - ((tid / width) * width);
-                    if (tid < numPixels) {
-                        int val = data[row * width + column];
-                        if (val != 0)
-                            atomicAdd(&histogram[val], 1);
-                    }
-                    return;
-                }           
-            ''', 'calc_hist')
-    data = cp.array(image_file.shape[1]) # FIXME
-    numPixels = image_file.shape[0] * image_file.shape[1]
-    histogram = cp.zeros_pinned(256)
-    # y(data, )
+# def kernel_calc_histogram(image_file, L):
+#     import cupy as cp
+#     y = cp.RawKernel(r'''
+#                 __global__ void calcHistogram(unsigned char *data, int width, int numPixels, int *histogram) {
+#                     int tid = blockDim.x * blockIdx.x + threadIdx.x;
+#                     int row = tid / width;
+#                     int column = tid - ((tid / width) * width);
+#                     if (tid < numPixels) {
+#                         int val = data[row * width + column];
+#                         if (val != 0)
+#                             atomicAdd(&histogram[val], 1);
+#                     }
+#                     return;
+#                 }           
+#             ''', 'calc_hist')
+#     data = cp.array(image_file.shape[1]) # FIXME
+#     numPixels = image_file.shape[0] * image_file.shape[1]
+#     histogram = cp.zeros(256)
+#     # y(data, )
 
 # TODO
 def calc_histogram(image_file):
@@ -90,6 +91,77 @@ def calc_histogram(image_file):
 
     return histogram
     
+# TODO translate my denoise code to work here if time
+# def denoise():
+#     cp.RawKernal = r'''__global__ void denoise(uchar *d_grey, uchar *d_output, int matrixHeight, int matrixWidth, int numPixels)
+#         {
+#             int col = blockIdx.x * blockDim.x + threadIdx.x;
+#             int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+#             unsigned char array[9];
+
+#             if(col < matrixWidth && row < matrixHeight)
+#             {
+#                 for(int x = 0; x < WINDOW_SIZE; x++)
+#                 {
+#                     for(int y = 0; y < WINDOW_SIZE; y++)
+#                     {
+#                         array[x*WINDOW_SIZE+y] = d_grey[(row+x-1)*matrixWidth+(col+y-1)];
+#                     }
+#                 }
+#                 // // write value to d_output
+#                 // d_output[rgb_ab] = (unsigned char) array[4];
+
+#                 // bubblesort works ...
+#                 for (int i = 0; i < 9; i++) {
+#                     for (int j = i + 1; j < 9; j++) {
+#                         if (array[i] > array[j]) { 
+#                             //Swap the variables.
+#                             unsigned char temp = array[i];
+#                             array[i] = array[j];
+#                             array[j] = temp;
+#                         }
+#                     }
+#                 }
+#                 d_output[rgb_ab] = (unsigned char) array[4];
+#             }
+#         }'''
+
+def macaroni(img):
+    
+    row, col = img.shape
+    
+    # add salt '255'
+    num_pixels_white = random.randint(0, img.size)
+    
+    img_copy = np.copy(img)
+    
+    for i in range(num_pixels_white):
+                 
+        # Pick a random x coordinate
+        x=random.randint(0, col - 1)
+                   
+        # Pick a random y coordinate
+        y=random.randint(0, row - 1)
+
+        # Color that pixel to white
+        img_copy[y][x] = 255
+         
+    # add pepper '0'
+    number_of_pixels_black = random.randint(0, img.size)
+    
+    for i in range(number_of_pixels_black):
+                 
+        # Pick a random x coordinate
+        x=random.randint(0, col - 1)
+                   
+        # Pick a random y coordinate
+        y=random.randint(0, row - 1)
+
+        # Color that pixel to black
+        img_copy[y][x] = 0
+         
+    return img_copy
 
 # TODO remember to make copies and work on those, DO NOT WORK ON OG IMAGES
 def main():
@@ -98,14 +170,16 @@ def main():
     config_file = "config.yaml"
     safe_conf = read_yaml(config_file)
     
-    # data_loc = Path(safe_conf['DATA_DIR'])
-    data_loc = Path(safe_conf['WIN_DATA_DIR'])
+    data_loc = Path(safe_conf['DATA_DIR'])
+    # data_loc = Path(safe_conf['WIN_DATA_DIR'])
     
     # glob("*.bmp") uses regex which slows this down, nothing else is in the dir so dont need to use it
     files = list(data_loc.iterdir())
     
     # create the output dir where all of the modified images will go
-    Path(safe_conf["WIN_OUTPUT_DIR"]).mkdir(parents=True, exist_ok=True)
+    Path(safe_conf['OUTPUT_DIR']).mkdir(parents=True, exist_ok=True)
+    # Path(safe_conf["WIN_OUTPUT_DIR"]).mkdir(parents=True, exist_ok=True)
+
     
     # reads image into np array
     # TODO make this operate in batch/iterate through files
@@ -113,7 +187,7 @@ def main():
     
     # convert to greyscale / proper color channel
     img = convert_image_to_single_channel(color_image, safe_conf['SELECTED_COLOR_CHANNEL'])
-    
+
     # these only work on my PC/desktop
     if(platform.system() != "Darwin"):
         # import GPUtil to check for GPU
@@ -124,19 +198,25 @@ def main():
         print(isGPU)
         # hist = kernel_calc_histogram(img, len(img))
     else:
+        ts = time.perf_counter()
         hist = calc_histogram(img)
+        te = time.perf_counter()
     
-    ts = time.perf_counter()
-    hist = calc_histogram(img)
-    te = time.perf_counter()
+    # ts = time.perf_counter()
+    # # hist = calc_histogram(img)
+    # te = time.perf_counter()
     
     timings = []
     timings.append(te - ts)
     
     plt.hist(hist, bins=256, range=(0,256))
     plt.title("cyl01.BMP")
-    plt.savefig(safe_conf["WIN_OUTPUT_DIR"] + "cyl01.png")
+    plt.savefig(safe_conf["OUTPUT_DIR"] + "cyl01.png")
+    # plt.savefig(safe_conf["WIN_OUTPUT_DIR"] + "cyl01.png")
     plt.close()
+    
+    # add salt & pepper noise to images
+    copy_img = macaroni(img)
     
 
 if __name__ == "__main__":
