bbf703b764594843dc87629d97f9f49176c5f508
batmaninrobin <malikmui@vcu.edu>
2022-05-06 16:11:41 -0600

    extracting features

--- classification.py
+++ classification.py
@@ -15,12 +15,65 @@ from main import (
 
 from part2 import (
     hist_threshold,
-    balance_hist
+    balance_hist,
+    erosion,
+    dilation
 )
 
 def save_image(img, filename, applied_method):
     new_img = Image.fromarray(img).convert("L")
     new_img.save(safe_conf["OUTPUT_DIR"] + filename + applied_method + ".jpg", format="JPEG")
+
+def shrinking_and_growing(hist_threshold):
+    
+    eroded = erosion(hist_threshold)
+    dilated = dilation(hist_threshold)
+    
+    sng = dilation(eroded)
+    
+    return sng, eroded, dilated
+
+
+# https://stackoverflow.com/questions/50313114/what-is-the-entropy-of-an-image-and-how-is-it-calculated
+def entropy(img, hist):
+    
+    # marg calculates marginal dist, then filter out probs == 0
+    marg = hist / img.size
+    marg = np.array(list(filter(lambda p: p > 0, marg)))
+    
+    # then sum whats left
+    entropy = -np.sum(np.multiply(marg, np.log2(marg)))
+
+    return entropy
+
+def area(sng):
+    unique, counts = np.unique(sng, return_counts=True)
+    counter = dict(zip(unique, counts))
+
+    black_pixel_count = counter[0]
+
+    return black_pixel_count
+
+
+def extract_features(img, hist, sng):
+    
+    # calculate the randomness of the pixel intensities
+    x1 = entropy(img, hist)
+    
+    # calculate mean of img histogram
+    x2 = np.mean(hist)
+    
+    # calculate area
+    x3 = area(sng)
+    
+    return {
+        "features": [x1, x2, x3]
+    }
+
+
+
+
+
 
 
 def main():
@@ -35,6 +88,8 @@ def main():
     files = list(data_loc.iterdir())
     filenames = [i for i in range(len(files))]
     
+    features = []
+    
     for i in range(len(files)):
         filenames[i] = os.path.basename(files[i])
         
@@ -44,6 +99,7 @@ def main():
         # format cli output
         print(filenames[i])
         
+        
         color_image = read_image(files[i])
         img = convert_image_to_single_channel(color_image, safe_conf['SELECTED_COLOR_CHANNEL'])
         
@@ -51,6 +107,16 @@ def main():
         
         histogram_threshold = hist_threshold(img, img_hist)
         save_image(histogram_threshold, filenames[i], "_hist_threshold")
+        
+        sng, eroded, dilated = shrinking_and_growing(histogram_threshold)
+        ## TODO dont need erosion and dilation just testing them
+        save_image(eroded, filenames[i], "_erosion")
+        save_image(dilated, filenames[i], "_dilation")
+        save_image(sng, filenames[i], "_sng")
+        
+        img_features = extract_features(img, img_hist, sng)
+        
+        # TODO append features and create a dataset
         
         
         
